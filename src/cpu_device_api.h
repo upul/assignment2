ef183fdf2af234813be23ff152cb3cb5773bdbda
Upul Bandara <upulbandara@gmail.com>
2017-06-06 01:13:35 +0530

    assignment completed

--- src/cpu_device_api.h
+++ src/cpu_device_api.h
@@ -14,17 +14,16 @@
 namespace dlsys {
 namespace runtime {
 
-class CPUDeviceAPI : public DeviceAPI {
+class CPUDeviceAPI: public DeviceAPI {
 public:
-  void *AllocDataSpace(DLContext ctx, size_t size, size_t alignment) final;
+	void *AllocDataSpace(DLContext ctx, size_t size, size_t alignment) final;
 
-  void FreeDataSpace(DLContext ctx, void *ptr) final;
+	void FreeDataSpace(DLContext ctx, void *ptr) final;
 
-  void CopyDataFromTo(const void *from, void *to, size_t size,
-                      DLContext ctx_from, DLContext ctx_to,
-                      DLStreamHandle stream) final;
+	void CopyDataFromTo(const void *from, void *to, size_t size,
+			DLContext ctx_from, DLContext ctx_to, DLStreamHandle stream) final;
 
-  void StreamSync(DLContext ctx, DLStreamHandle stream) final;
+	void StreamSync(DLContext ctx, DLStreamHandle stream) final;
 };
 
 } // namespace runtime
